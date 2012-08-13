9cde5cac346a8ec7724c9a179bc8a079c0db8a54
clombardot <cedric.lombardot@gmail.com>
2012-08-13 12:10:25 +0200

    Mirror of https://bitbucket.org/glorpen/glorpenpropeleventbundle/issues?status=new&status=open

---
+++ PropelEventBundle/Connection/EventPropelPDO.php
@@ -0,0 +1,34 @@
+<?php
+namespace Glorpen\PropelEvent\PropelEventBundle\Connection;
+
+use Glorpen\PropelEvent\PropelEventBundle\Events as PropelEvents;
+use \PropelPDO;
+
+/**
+ * Connection class with transaction events.
+ * Can trigger connection.commit and connection.rollback events.
+ * @author Arkadiusz Dzięgiel
+ */
+class EventPropelPDO extends PropelPDO
+{
+    public function commit()
+    {
+        $opcount = $this->getNestedTransactionCount();
+        $return = parent::commit();
+
+        if($return && $opcount === 1) EventDispatcherProxy::trigger(new PropelEvents\ConnectionEvent($this), 'connection.commit');
+
+        return $return;
+    }
+
+    public function rollBack()
+    {
+        $opcount = $this->getNestedTransactionCount();
+        $return = parent::rollBack();
+
+        if($return && $opcount === 1) EventDispatcherProxy::trigger(new PropelEvents\ConnectionEvent($this), 'connection.rollback');
+
+        return $return;
+    }
+
+}
